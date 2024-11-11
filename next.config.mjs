c3d7ce908ea4c4278c93656649c7231cccb720bc
Hybola <keeratipong.b@gmail.com>
2024-11-11 15:16:12 +0700

    lecture 502: fetch cabins

--- next.config.mjs
+++ next.config.mjs
@@ -5,7 +5,7 @@ const nextConfig = {
     remotePatterns: [
       {
         protocol: "https",
-        hostname: "https://joqbbroyexvmfpmvapgd.supabase.co",
+        hostname: "joqbbroyexvmfpmvapgd.supabase.co",
         port: "",
         pathname: "/storage/v1/object/public/cabin-images/**",
       },
