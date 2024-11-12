968833a61f002898526400de7982dec63ab2410b
Hybola <keeratipong.b@gmail.com>
2024-11-12 18:40:41 +0700

    lecture 503: submit contact form

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -3,24 +3,25 @@ import { supabase } from "@/lib/supabase";
 export default async function handler(req, res) {
   if (req.method !== "POST")
     return res.status(405).json({
-      sucess: false,
+      success: false,
       message: "405: method is not allow. Please make a POST request",
     });
-  const contactData = {
-    fullName: "Jonas",
-    email: "test",
-    subject: "booking",
-    message: "Hey",
-  };
+  //   const contactData = {
+  //     fullName: "Jonas",
+  //     email: "test",
+  //     subject: "booking",
+  //     message: "Hey",
+  //   };
+  const contactData = JSON.parse(req.body);
   const { error } = await supabase.from("contact").insert([contactData]);
   if (error)
     return res.status(500).json({
-      sucess: false,
+      success: false,
       message: "Could not send your message, please try again",
     });
 
   res.status(200).json({
-    sucess: true,
+    success: true,
     message: "Thanks for your message! We will be in touch soon:)",
   });
 }
